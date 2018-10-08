1dbc8d3920a685e1cb365a5d4007ada06f0b1ab2
Martynas <packmaster@riawolf.com>
2018-10-08 21:20:29 +0300

    pakeiciau pavadinima is a i geresni

--- src/lt/kaunascoding/Main.java
+++ src/lt/kaunascoding/Main.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+
+
 	// write your code here
         System.out.println("Labas, iveskite skaičiu");
         Scanner sc = new Scanner(System.in);
