2ff04cf591ea6f3e97651ef8d0a2011053c64776
Martynas <packmaster@riawolf.com>
2018-10-08 21:20:16 +0300

    pakeiciau pavadinima is a i geresni

--- src/lt/kaunascoding/Main.java
+++ src/lt/kaunascoding/Main.java
@@ -3,12 +3,16 @@ package lt.kaunascoding;
 import java.util.Scanner;
 
 public class Main {
+// sukurti  nauja branch kuriame butu igyvendintas metodas ivesto skaiciaus kelimui kvadratu
+    // panaudoti feature branch, sukurti pull request, ir atlikti merge
+
 
     public static void main(String[] args) {
-	// write your code here
+
         System.out.println("Labas, iveskite skaičiu");
         Scanner sc = new Scanner(System.in);
         int ivestasSkaicius = sc.nextInt();
+
         System.out.println("Ivestas skaičius: "+ ivestasSkaicius);
     }
 }
