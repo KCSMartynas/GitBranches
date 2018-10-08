8e791969885c77d96311fd81e0f43994570b85ce
Martynas <packmaster@riawolf.com>
2018-10-08 20:16:49 +0300

    Veikaintis skaicu nuskaitymas ir ju isvedimas i ekrana

--- src/lt/kaunascoding/Main.java
+++ src/lt/kaunascoding/Main.java
@@ -1,9 +1,14 @@
 package lt.kaunascoding;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Labas, iveskite skaičiu");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("Ivestas skaičius: "+ a);
     }
 }
